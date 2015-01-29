34809f40848181e1858c652f8efe3961b3d5cbcd
Ilan Kleiman <ilankleiman@gmail.com>
2015-01-29 10:16:33 -0500

    Clicking hamburger will close menu
    ugly, but working way of closing the menu by clicking the hamburger.

--- js/hamburger.js
+++ js/hamburger.js
@@ -14,28 +14,47 @@ jQuery(document).ready(function () {
     //Open the menu
     jQuery("#hamburger").click(function () {
 
+    var menu_action = localStorage.getItem('menu_action');
+    if(menu_action == "open")
+    {
+    	var menu_action = "open_d";
+    }
+    else
+    {
+    	var menu_action = "closed_d";
+    }
+    if(menu_action == "closed_d")
+    {
+        jQuery('#header_title').fadeToggle('fast');
         jQuery('#content').css('min-height', jQuery(window).height());
-
         jQuery('nav').css('opacity', 1);
-
-        //set the width of primary content container -> content should not scale while animating
         var contentWidth = jQuery('#content').width();
-
-        //set the content with the width that it has originally
         jQuery('#content').css('width', contentWidth);
-
-        //display a layer to disable clicking and scrolling on the content while menu is shown
         jQuery('#contentLayer').css('display', 'block');
-
-        //disable all scrolling on mobile devices while menu is shown
         jQuery('#container').bind('touchmove', function (e) {
             e.preventDefault()
-        });
-
-        //set margin for the whole container with a jquery UI animation
+    });
         jQuery("#container").animate({"marginLeft": ["70%", 'easeOutExpo']}, {
-            duration: 700
-        });
+        duration: 700
+    });
+        localStorage.setItem('menu_action', 'open');
+    }
+    if(menu_action == "open_d")
+    {
+        jQuery('#header_title').fadeToggle('fast');
+        jQuery('#container').unbind('touchmove');
+        jQuery("#container").animate({"marginLeft": ["-1", 'easeOutExpo']}, {
+        duration: 700,
+        complete: function () {
+            jQuery('#content').css('width', 'auto');
+            jQuery('#contentLayer').css('display', 'none');
+            jQuery('nav').css('opacity', 0);
+            jQuery('#content').css('min-height', 'auto');
+    }
+    });
+    localStorage.setItem('menu_action', 'closed');
+    
+    }
 
     });
 
